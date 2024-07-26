6e98fd9bb57a42f73ea6c357a083af468ca9ddba
James Brown <jamesbrown173@gmail.com>
2024-07-26 10:14:40 +0700

    '

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("form");
+const billInput = document.getElementById('bill')
+
+console.log(form);
+
+
+// Listen all possible ways of triggering
+
+// 1. Type in the bill
+billInput.addEventListener()
+form.oninput = function()
+
+// 2. Click a button
+
+// 3. Type in the custom tip field
+
+// 4. Type in the number of people field
+
+// 5. Click the reset button
+
+// Each time any of 1 - 4 is done should submit GET the whole form data
+
+// If 5 is done reset the form to blank values
+
+// If bill or number of people is empty do nothing
+
+// If Custom Tip is Typed in then remove any active states from buttons and calculcate (BILL / C.TIP) / PEOPLE
+
+// If Tip Button is pressed then calculate (BILL / TIP BUTTON VALUE) / PEOPLE
+
+// The functions should update the Tip and Total Amount elements
+
+// Reset button should reset the form.
