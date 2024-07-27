bc80ac17cc2abf00b1ccc050b9903c7a98cedeaa
James Brown <jamesbrown173@gmail.com>
2024-07-27 09:47:12 +0700

    .

--- src/main.js
+++ src/main.js
@@ -1,33 +1,91 @@
 const form = document.querySelector("form");
-const billInput = document.getElementById('bill')
+const billInput = document.getElementById("bill");
+const customTipInput = document.getElementById("customTip");
+const tipButtons = document.getElementsByClassName("tipButton");
+const numPeopleInput = document.getElementById("numPeople");
+const resetButton = document.getElementById("clearAll");
+const resultTipAmount = document.getElementById("resultTip");
+console.log(resultTipAmount);
+const resultTotalAmount = document.getElementById("resultTotal");
 
-console.log(form);
+/*================================================================================================================
+currency formatter
+*==============================================================================================================*/
 
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
-// Listen all possible ways of triggering
+/*================================================================================================================
+variables
+*==============================================================================================================*/
+var billAmount = 0;
+var tipPercentage = 0;
+var numPeople = 0;
+
+/*================================================================================================================
+1. list all user input methods
+2. get all form data and assign to variables
+4. reset the form
+*==============================================================================================================*/
+
+// List all possible ways of triggering
 
 // 1. Type in the bill
-billInput.addEventListener()
-form.oninput = function()
+billInput.addEventListener("keyup", function (event) {
+  billAmount = event.target.value;
+  calculateAndUpdate(billAmount, tipPercentage, numPeople);
+});
 
 // 2. Click a button
+for (let i = 0; i < tipButtons.length; i += 1) {
+  // Loop over the buttons as there are multiple
+  tipButtons[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    tipPercentage = event.target.value;
+
+    calculateAndUpdate(billAmount, tipPercentage, numPeople);
+  });
+}
 
 // 3. Type in the custom tip field
+customTipInput.addEventListener("keyup", function (event) {
+  tipPercentage = event.target.value / 100;
+  calculateAndUpdate(billAmount, tipPercentage, numPeople);
+});
 
 // 4. Type in the number of people field
+numPeopleInput.addEventListener("keyup", function (event) {
+  numPeople = event.target.value;
+  calculateAndUpdate(billAmount, tipPercentage, numPeople);
+});
 
 // 5. Click the reset button
+resetButton.addEventListener("click", function () {
+  form.reset();
+  billAmount = 0;
+  tipPercentage = 0;
+  numPeople = 0;
+  resultTipAmount.innerText = "$0.00";
+  resultTotalAmount.innerText = "$0.00";
 
-// Each time any of 1 - 4 is done should submit GET the whole form data
+  console.log("clicked reset");
+});
 
-// If 5 is done reset the form to blank values
+/*================================================================================================================
+1. calculateAndUpdate
 
-// If bill or number of people is empty do nothing
+2. update the elements
+*==============================================================================================================*/
 
-// If Custom Tip is Typed in then remove any active states from buttons and calculcate (BILL / C.TIP) / PEOPLE
+function calculateAndUpdate(bill, tip, people) {
+  totalAmountPerPerson = bill / people;
+  tipPerPerson = (bill * tip) / people;
 
-// If Tip Button is pressed then calculate (BILL / TIP BUTTON VALUE) / PEOPLE
+  resultTipAmount.innerText = `$ ${tipPerPerson}`;
+  resultTotalAmount.innerText = `$ ${totalAmountPerPerson}`;
 
-// The functions should update the Tip and Total Amount elements
-
-// Reset button should reset the form.
+  console.log(`Total Amount Per Person: ${totalAmountPerPerson}`);
+  console.log(`Tip Per Person: ${tipPerPerson}`);
+}
